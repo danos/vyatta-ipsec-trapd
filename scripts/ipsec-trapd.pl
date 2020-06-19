danos/vyatta-ipsec-trapd

commit 7ee2a3dbdeb900e32645c3346d9087012e7f898b

Align to latest xfrm monitor output
The regex used to match the SA creation/deletion/update lines
from "ip -o xfrm monitor" no longer matched.

Inbound SAs no longer get added, but updated by the IKE control-plane.
So "Updated" lines can no longer be ignored.

VRVDR-51643

scripts/ipsec-trapd.pl
@@ -1,7 +1,7 @@
 #!/usr/bin/perl
 #
 # Copyright (c) 2015, Brocade Communications Systems, Inc.
-# Copyright (c) 2018, AT&T Intellectual Property.
+# Copyright (c) 2018, 2020, AT&T Intellectual Property.
 # All rights reserved.
 #
 # SPDX-License-Identifier: GPL-2.0-only
@@ -275,9 +275,9 @@ sub parse
     my $buffer = shift;
 
     # IKE key events, ignore
-    return if (substr($buffer, 0, 8) eq "Expired " || substr($buffer, 0, 8) eq "Updated ");
+    return if (substr($buffer, 0, 8) eq "Expired ");
 
-    $buffer =~ /src ([^ ]+) dst ([^ ]+) proto (ah|esp) spi (0x[0-9a-f]{8}) .* mode (tunnel|transport) /;
+    $buffer =~ /src ([^ ]+) dst ([^ ]+)\\.* proto (ah|esp) spi (0x[0-9a-f]{8}) .* mode (tunnel|transport)\\/;
     return unless (defined $1);
 
     my $sa = {};

commit cb5378b5aa667195aad85414c3df69716020d400

Restore original match regex
The additonal match for the backspace is unnecssary, since those
get stripped earlier in the process.

Resolves regression introduced by:
7ee2a3dbdeb900e32645c3346d9087012e7f898b

VRVDR-51643

scripts/ipsec-trapd.pl
@@ -277,7 +277,7 @@ sub parse
     # IKE key events, ignore
     return if (substr($buffer, 0, 8) eq "Expired ");
 
-    $buffer =~ /src ([^ ]+) dst ([^ ]+)\\.* proto (ah|esp) spi (0x[0-9a-f]{8}) .* mode (tunnel|transport)\\/;
+    $buffer =~ /src ([^ ]+) dst ([^ ]+) proto (ah|esp) spi (0x[0-9a-f]{8}) .* mode (tunnel|transport) /;
     return unless (defined $1);
 
     my $sa = {};